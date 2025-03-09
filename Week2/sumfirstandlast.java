e6fbd8ca96d570aed598dc3ea6aeb11e58aa179a
brahana <brahanarama@gmail.com>
2025-03-09 22:17:06 +0530

    week 3 gfg problems completed

--- Week2/sumfirstandlast.java
+++ Week2/sumfirstandlast.java
@@ -8,14 +8,27 @@ int ld;
 int sum=0;
 
 ld=n%10;
+
+if(n<10)
+{
+    System.out.println(n);
+}
+
+else{
         while(n>=10)
         {
             
             n=n/10;
+            
+     
+
+   
         }
-
         sum=n+ld;
-
         System.out.println(sum);
+   
     }
+}
+    
+
 }
